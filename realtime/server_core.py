3f59d487b143bd4494d43e7d1978e073c67e492e
whth <whvod222@naver.com>
2025-12-24 01:16:18 +0900

    카메라 위치 추가

--- realtime/server_core.py
+++ realtime/server_core.py
@@ -125,11 +125,13 @@ class RealtimeServer:
 
         self._ui_cameras_on_map = [
             {"id": "camMarker-1", "cameraId": "cam-1", "x": -0.01, "y": 0.5},
-            {"id": "camMarker-3", "cameraId": "cam-2", "x": 1.01, "y": 0.5},
+            {"id": "camMarker-2", "cameraId": "cam-2", "x": 0.5, "y": -0.01},
+            {"id": "camMarker-3", "cameraId": "cam-3", "x": 1.01, "y": 0.5},
         ]
         self._ui_cameras_status = [
             {"id": "cam-1", "name": "Camera 1", "streamUrl": "http://192.168.0.101:8080/stream"},
-            {"id": "cam-2", "name": "Camera 3", "streamUrl": "http://192.168.0.106:8080/stream"},
+            {"id": "cam-2", "name": "Camera 2", "streamUrl": "http://192.168.0.101:8081/stream"},
+            {"id": "cam-3", "name": "Camera 3", "streamUrl": "http://192.168.0.106:8080/stream"},
         ]
         self._ui_cam_status: Optional[dict] = None
         self._ui_obstacle_map: Dict[str, dict] = {}
